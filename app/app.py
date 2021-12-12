ce93a53da2ab6ea45ac7c62c7366f20d08695840
Matt Hergott <matt@miabellaai.net>
2021-12-12 12:42:15 -0700

    Add files via upload

--- app/app.py
+++ app/app.py
@@ -1,1 +1,29 @@
+from functions.Translator import Translator
+from functions.Summarizer import Summarizer
 
+from functions.translate_summarize import translate_summarize
+
+import json
+
+
+def handler(event, context):
+
+    de = 'https://www.deutschland.de/de/feed-news/rss.xml'
+    feed_url = de
+
+    repetitions = 1
+
+    t5_version = 't5-base'
+
+    translator = Translator()
+    summarizer = Summarizer(t5_version=t5_version)
+
+    for _ in range(repetitions):
+        res = translate_summarize(
+            feed_url, translator=translator, summarizer=summarizer, max_chars=25000)
+
+        print(res)
+
+    return {'headers': {'Content-Type': 'application/json'}, 'statusCode': 200, 'body': json.dumps({"message": res, "event": event})}
+
+# handler(event='', context='')
